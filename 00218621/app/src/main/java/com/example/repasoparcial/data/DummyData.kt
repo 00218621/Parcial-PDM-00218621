731ddb0b3be2cf016b5a86973540cbc53d9a1a65
00218621 <00218621@uca.edu.sv>
2023-05-23 09:19:24 -0600

    Capa model finish

---
+++ 00218621/app/src/main/java/com/example/repasoparcial/data/DummyData.kt
@@ -0,0 +1,15 @@
+package com.example.repasoparcial.data
+
+import com.example.repasoparcial.data.model.ActorModel
+
+//TODO: Data quemada
+val name = "Luis ALfonso Medina"
+val age = "27"
+
+val name1 = "William Perdomo Anaya"
+val age1 = "32"
+
+val Actors= mutableListOf(
+    ActorModel(name,age),
+    ActorModel(name1, age1)
+)
---
+++ 00218621/app/src/main/java/com/example/repasoparcial/data/model/ActorModel.kt
@@ -0,0 +1,6 @@
+package com.example.repasoparcial.data.model
+
+data class ActorModel (
+    val name: String,
+    val age: String
+        )
